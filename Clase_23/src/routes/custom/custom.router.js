31be0a5382fbf96555ee0bbb8cf260003b56aea2
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-01-26 16:34:50 -0300

    After - Custom Router

--- Clase_23/src/routes/custom/custom.router.js
+++ Clase_23/src/routes/custom/custom.router.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { PRIVATE_KEY } from "../../utils.js";
 
 
 export default class CustomRouter {
@@ -12,11 +14,87 @@ export default class CustomRouter {
     };
     init() { }; //Esta inicialilzacion se usa para las clases heredadas.
 
-    get(path, ...callbacks) {
+    get(path, policies, ...callbacks) {
         console.log("Entrando por GET a custom router con Path: " + path);
-        this.router.get(path, this.applyCallbacks(callbacks))
+        console.log(policies);
+        this.router.get(
+            path,
+            this.handlePolicies(policies),
+            this.generateCustomResponses,
+            this.applyCallbacks(callbacks))
     }
 
+    // POST
+    post(path, policies, ...callbacks) {
+        this.router.post(path,
+            this.handlePolicies(policies),
+            this.generateCustomResponses,
+            this.applyCallbacks(callbacks));
+    };
+
+
+    // PUT
+    put(path, policies, ...callbacks) {
+        this.router.put(path,
+            this.handlePolicies(policies),
+            this.generateCustomResponses,
+            this.applyCallbacks(callbacks));
+    };
+
+
+    // DELETE
+    delete(path, policies, ...callbacks) {
+        this.router.delete(path,
+            this.handlePolicies(policies),
+            this.generateCustomResponses,
+            this.applyCallbacks(callbacks));
+    };
+
+    handlePolicies = policies => (req, res, next) => {
+        console.log("Politicas a evaluar:");
+        console.log(policies);
+
+        //Validar si tiene acceso publico:
+        if (policies[0] === "PUBLIC") return next();
+
+        //El JWT token se guarda en los headers de autorización.
+        const authHeader = req.headers.authorization;
+        console.log("Token present in header auth:");
+        console.log(authHeader);
+
+        if (!authHeader) {
+            return res.status(401).send({ error: "User not authenticated or missing token." });
+        }
+
+        const token = authHeader.split(' ')[1]//Se hace el split para retirar la palabra Bearer.
+
+        // Validamos token
+        jwt.verify(token, PRIVATE_KEY, (error, credential) => {
+            if (error) return res.status(403).send({ error: "Token invalid, Unauthorized!" });
+
+            // Token ok
+            const user = credential.user
+
+            // Preguntamos si dentro del array policies se encuentra el user.role que me esta llegando con este usuario
+            if (!policies.includes(user.role.toUpperCase())) return res.status(403).send({ error: "El usuario no tiene privilegios, revisa tus roles!" });
+
+            // si el user.role se encuentra dentro de policies, podes ingresar
+            req.user = user;
+            console.log(req.user);
+            next()
+        })
+    }
+
+
+    generateCustomResponses = (req, res, next) => {
+        //Custom responses 
+        res.sendSuccess = payload => res.status(200).send({ status: "Success", payload });
+        res.sendInternalServerError = error => res.status(500).send({ status: "Error", error });
+        res.sendClientError = error => res.status(400).send({ status: "Client Error, Bad request from client.", error });
+        res.sendUnauthorizedError = error => res.status(401).send({ error: "User not authenticated or missing token." });
+        res.sendForbiddenError = error => res.status(403).send({ error: "Token invalid or user with no access, Unauthorized please check your roles!" });
+        next()
+    }
 
 
     // función que procese todas las funciones internas del router (middlewares y el callback principal)
@@ -33,3 +111,4 @@ export default class CustomRouter {
         });
     };
 }
+
