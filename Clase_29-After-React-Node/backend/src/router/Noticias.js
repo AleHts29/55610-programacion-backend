3c85db70dc777e1659e2c89a5418ba198d11c41e
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-02-16 13:46:51 -0300

    c29-After

---
+++ Clase_29-After-React-Node/backend/src/router/Noticias.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import ControladorNoticias from '../controllers/Noticias.js';
+
+const router = express.Router();
+
+class RouterNoticias {
+    //TODO: a implementar
+    constructor() {
+        this.controladorNoticias = new ControladorNoticias();
+    }
+
+    start() {
+        router.get('/:id', this.controladorNoticias.obtenerNoticias)
+        router.post('/', this.controladorNoticias.guardarNoticias);
+        router.put('/:id', this.controladorNoticias.actualizarNoticia);
+        router.delete('/:id', this.controladorNoticias.borrarNoticia);
+
+        return router;
+    }
+}
+
+export default RouterNoticias;
