c3906554552b4bd0f2e57856fe7f832403dee362
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-02-19 10:29:57 -0300

    clase30

---
+++ Clase_30/src/controllers/email.controller.js
@@ -0,0 +1,5 @@
+import nodemailer from 'nodemailer';
+import config from '../config/config.js';
+import __dirname from '../utils.js'
+
+// TODO::: a implementar
---
+++ Clase_30/src/controllers/sms.controller.js
@@ -0,0 +1,5 @@
+import BusinessService from "../services/dao/business.dao.js";
+import config from "../config/config.js";
+import twilio from 'twilio';
+
+// TODO::: a implementar
