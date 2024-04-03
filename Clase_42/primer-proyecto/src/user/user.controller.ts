b130eb7f7b58b2b10f90ef28fe04a2a75c3d0935
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-04-03 12:19:53 -0300

    clase42

---
+++ Clase_42/primer-proyecto/src/user/user.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    const users = this.userService.findAll();
+    return { status: 'Success', payload: users }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // Definimos una excepcion
+    if (isNaN(+id)) throw new HttpException("El id debe ser numero", HttpStatus.BAD_REQUEST)
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
